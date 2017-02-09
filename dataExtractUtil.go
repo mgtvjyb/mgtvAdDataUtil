f9f8edef70ba85a668a294f8225e8a7441830328
jinyibin <yibin2@mgtv.com>
2017-02-09 18:42:16 +0800

    add extract util

---
+++ dataExtractUtil.go
@@ -0,0 +1,121 @@
+package mgtvAdDataUtil
+
+import (
+	"config"
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+)
+
+var host = "ac1c27a32073d50c494243b4b7e33f56"
+var user = "29307463389dbc8e53c75b69b5b5d325"
+var passwd = "d1393a5e5f6329d8ae04c21daddb1211"
+var port = "3c2902cc457e4dcc40e39ca2c7c64c18"
+var dbname = "53547354c18777d63fee16738ba7fbf7"
+
+var extract_hids = make([]string, 0)
+var lock = sync.RWMutex
+
+// var program_ids = []string{""}
+
+/*
+	初始化需要提取的合集id
+	例如：分类的节目id，SA级，运营重点关注的几个合集，以及top100合集
+*/
+func isCollectionIdNeed(aeskey, hid string, date string) bool {
+	lock.Lock()
+	if len(extract_hids) == 0 {
+		initCollectionIds(date)
+	}
+	lock.Unlock()
+	if Contains(extract_hids, hid) {
+		return true
+	}
+	return false
+}
+func initCollectionIds(date string, aeskey string) {
+	dec_user, _ := AesCBCDecrypte(user, aeskey)
+	dec_host, _ := AesCBCDecrypte(host, aeskey)
+	dec_passwd, _ := AesCBCDecrypte(passwd, aeskey)
+	dec_dbname, _ := AesCBCDecrypte(dbname, aeskey)
+	dec_port, _ := AesCBCDecrypte(port, aeskey)
+
+	db, err := sql.Open("mysql", dec_user+":"+dec_passwd+"@tcp("+dec_host+":"+dec_port+")/"+dec_dbname)
+	if err != nil {
+		fmt.Println("open mysql error")
+		return
+	}
+
+	defer db.Close()
+
+	loc, _ := time.LoadLocation("Local")
+	cur_time, _ := time.ParseInLocation("20060102", date, loc)
+	unix_cur := cur_time.Unix()
+	sab_sql := fmt.Sprintf("select c_name,c_id,grade from amp.collection_grade where index_time=%d", unix_cur)
+	rows, err := db.Query(sab_sql)
+	if err != nil {
+		fmt.Println("get sabinfo error date:", date, " error: ", err)
+		return
+	}
+	result := getResult(rows)
+	for _, row := range result {
+		cid := row[1]
+		if Contains(extract_hids, cid) {
+			continue
+		}
+		extract_hids = append(extract_hids, cid)
+	}
+	// 查top100的合集id
+	top_sql := fmt.Sprintf("select hid from stock_history.hid_req_count order by count desc limit 100")
+	rows, err = db.Query(top_sql)
+	if err != nil {
+		fmt.Println("get top hid 100 error date:", date, " error: ", err)
+		return
+	}
+	result = getResult(rows)
+	for _, row := range result {
+		hid := row[0]
+		if Contains(extract_hids, hid) {
+			continue
+		}
+		extract_hids = append(extract_hids, hid)
+	}
+}
+
+func getResult(rows sql.Rows) [][]string {
+	cols, err := rows.Columns()
+	if err != nil {
+		fmt.Println("query mysql error get rows columns", err)
+		return nil, err
+	}
+	values := make([]interface{}, len(cols))
+	for i, _ := range cols {
+		var ii interface{}
+		values[i] = &ii // scan的每一个元素都必须是个引用
+	}
+	var result = make([][]string, 0)
+	for rows.Next() {
+		rows.Scan(values...)
+		row := make([]string, 0)
+		for _, v := range values {
+			raw_value := *v.(*interface{})
+			b, _ := raw_value.([]uint8)
+			row = append(row, string(b))
+		}
+		result = append(result, row)
+	}
+	return result
+}
+
+func Contains(strList []string, str string) bool {
+	for _, strv := range strList {
+		if strv == str {
+			return true
+		}
+	}
+	return false
+}
