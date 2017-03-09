8fe06f64da77e65da5b7508cc1c4de942aed425f
jinyibin <yibin2@mgtv.com>
2017-03-09 15:43:33 +0800

    add redis

---
+++ redis.go
@@ -0,0 +1,37 @@
+package compute
+
+import (
+	"errors"
+	"fmt"
+	"gopkg.in/redis.v3"
+	"time"
+)
+
+var (
+	client RedisClient = nil
+)
+
+type RedisClient interface {
+	Del(keys ...string) *redis.IntCmd
+}
+
+func InitRedisClient(addr string, poolSize int, timeout time.Duration, password string) (RedisClient, error) {
+	clientR := redis.NewClient(&redis.Options{
+		Addr:         addr,
+		PoolSize:     poolSize,
+		ReadTimeout:  timeout,
+		WriteTimeout: timeout,
+		Password:     password,
+	})
+	_, err := clientR.Ping().Result()
+	if err != nil {
+		fmt.Println("init redis ping error", err)
+	}
+	client = clientR
+	fmt.Println("redis client-----------", client)
+	return client, err
+}
+
+func (*RedisClient) Del(key string) error {
+	return client.Del(key).Err()
+}
